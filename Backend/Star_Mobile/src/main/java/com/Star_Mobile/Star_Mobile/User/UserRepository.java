85ce7e4a0c4bfb73e6e5f3af9ce997a250ceb896
Theekshana-Vimukthi-Jayawickrama <152789878+Theekshana-Vimukthi-Jayawickrama@users.noreply.github.com>
2024-05-29 12:08:02 +0530

    User authentication has been created.

---
+++ Backend/Star_Mobile/src/main/java/com/Star_Mobile/Star_Mobile/User/UserRepository.java
@@ -0,0 +1,12 @@
+package com.Star_Mobile.Star_Mobile.User;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUserName(String userName);
+
+}
---
+++ Backend/Star_Mobile/src/main/java/com/Star_Mobile/Star_Mobile/auth/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.Star_Mobile.Star_Mobile.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String userName;
+    private String password;
+}
