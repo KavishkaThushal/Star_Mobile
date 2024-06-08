701bd557bf9ef1191190634ad6955b3c985962f0
kavishka <thushalrasingolla99@gmail.com>
2024-06-08 11:21:25 +0530

    store page done

--- Frontend/src/Pages/Store/Store.jsx
+++ Frontend/src/Pages/Store/Store.jsx
@@ -3,6 +3,9 @@ import Card from '../../Components/Card/Card'
 import { MdClose } from "react-icons/md";
 import './store.css'
 
+const Category=['Smart Phone','Earphones','Smart Watch']
+const Brand=['Redmi','Samsung','Infinix']
+const Price=['Under 20000Lkr','Under 50000Lkr','Under 100000Lkr']
 
 function Store() {
   const [filter, setFilter] = useState(false)
@@ -17,21 +20,15 @@ function Store() {
               <MdClose onClick={()=>setFilter((prev)=>!prev)} />
               <div className='filter-section'>
                 <h3>Category</h3>
+                {
+                  Category?.map((item,index)=>(
+                    <span key={index}>
+                      <input type='checkbox' id='category1' name='category1' value='category1'/>
+                      <p>{item}</p>
+                    </span>
+                  ))
+                }
                 
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Smart Phone</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Earphones</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Smart Watch</p>
-                </span>
                 </div>
 
                 <hr/>
@@ -39,20 +36,14 @@ function Store() {
                 <div className='filter-section'>
                 <h3>Brand</h3>
                 
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Redmi</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Samsung</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Infinix</p>
-                </span>
+                {
+                  Brand?.map((item,index)=>(
+                    <span key={index}>
+                      <input type='checkbox' id='category1' name='category1' value='category1'/>
+                      <p>{item}</p>
+                    </span>
+                  ))
+                }
                 </div>
 
                 <hr/>
@@ -60,20 +51,14 @@ function Store() {
                 <div className='filter-section'>
                 <h3>Price</h3>
                 
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Under 20000Lkr</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Under 50000Lkr</p>
-                </span>
-
-                <span>
-                <input type='checkbox' id='category1' name='category1' value='category1'/>
-                <p>Under 100000Lkr</p>
-                </span>
+                {
+                  Price?.map((item,index)=>(
+                    <span key={index}>
+                      <input type='checkbox' id='category1' name='category1' value='category1'/>
+                      <p>{item}</p>
+                    </span>
+                  ))
+                }
                 </div>
 
                 <hr/>
