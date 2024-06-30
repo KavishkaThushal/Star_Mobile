9a440c7670cca0355b5cbba1b46fad8cccb3d739
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-06-30 06:30:33 +0530

    Developed login page

--- Frontend/src/Pages/Login/Login.jsx
+++ Frontend/src/Pages/Login/Login.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import LoginForm from '../../Components/Authenticate/LoginForm'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <LoginForm></LoginForm>
+    </div>
   )
 }
 
