5fa7ce4f71f249d5431b9a9194cc592ed20710f2
W. Shashindu Chamika <126416494+WShashinduChamika@users.noreply.github.com>
2024-07-12 14:03:39 +0530

    Developed gallery page and updated nav bar

---
+++ Frontend/src/Components/Gallery/slider/Slider.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Slider.css'
+
+export default function Slider() {
+  return (
+    <div className='gallery-slider'>
+       <h1>Gallery</h1>
+    </div>
+  )
+}
--- Frontend/src/Pages/Gallery/Gallery.jsx
+++ Frontend/src/Pages/Gallery/Gallery.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Slider from '../../Components/Gallery/slider/Slider'
+import GalleryComponent from '../../Components/Gallery/gallery_content/GalleryContent'
 
 function Gallery() {
   return (
-    <div>Gallery</div>
+    <div>
+       <Slider></Slider>
+       <GalleryComponent></GalleryComponent>
+    </div>
   )
 }
 
