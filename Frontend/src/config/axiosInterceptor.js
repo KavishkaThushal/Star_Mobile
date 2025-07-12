548524607d11c4b587d9187007c6eaca4925aa05
Kavishka Thushal Rasingolla <kavishka.thushal@arimaclanka.com>
2025-07-12 21:34:31 +0530

    FIX:token fixed

--- Frontend/src/config/axiosInterceptor.js
+++ Frontend/src/config/axiosInterceptor.js
@@ -12,7 +12,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem('authToken');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
